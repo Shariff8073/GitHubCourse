65a43d6f20e5f085874b16f95f5376e7e88d265b
Adil <adilmohammed416@gmail.com>
2022-09-17 23:07:49 +0530

    Added new class file in Brach - Shariff

---
+++ src/test/testCase3.java
@@ -0,0 +1,11 @@
+package test;
+
+public class testCase3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Test case 3 is added");
+	}
+
+}
