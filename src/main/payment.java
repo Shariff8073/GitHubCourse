ad8454c73cb178e6710cb2f902966a980a47512b
Adil <adilmohammed416@gmail.com>
2022-09-21 22:32:40 +0530

    Payment section is developed

---
+++ src/main/payment.java
@@ -0,0 +1,11 @@
+package main;
+
+public class payment {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Developing payment page");
+	}
+
+}
